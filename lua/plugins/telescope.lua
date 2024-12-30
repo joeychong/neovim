ccb26689f88955325d47335a6c42d28b3dc15ba8
jckoh <chong1250_1999@yahoo.com>
2024-12-30 21:35:09 +0800

    use native fzf

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -60,13 +60,19 @@ return {
           --      do the following
           --   codeactions = false,
           -- }
-        }
+        }, fzf = {}
       }
     }
   }, {
     "nvim-telescope/telescope-ui-select.nvim",
     config = function()
       require("telescope").load_extension("ui-select")
+    end
+  }, {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'make',
+    config = function()
+      require('telescope').load_extension('fzf')
     end
   }
 }
