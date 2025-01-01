6f377a60b4ef9e3a0a6df199710fdd5625fa8394
jckoh <chong1250_1999@yahoo.com>
2025-01-01 15:00:30 +0800

    Add comments

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
+-- load lazy vim
 require("config.lazy")
 
+-- set vim behavior
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
@@ -8,10 +10,12 @@ vim.cmd("set shiftwidth=2")
 vim.opt.number = true
 -- vim.opt.relativenumber = true
 
+-- custom key mapping
 vim.keymap.set('n', '<C-s>', ':w<CR>', { noremap = true, silent = true })
 vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>a', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-t>', ':tabnew<CR>', { noremap = true, silent = true })
 
+-- enable markdown code block syntax lighlighting
 vim.g.markdown_fenced_languages = {
   "ts=typescript",
   "js=javascript",
@@ -24,4 +28,5 @@ vim.g.markdown_fenced_languages = {
   "yaml",
 }
 
+-- enable copy and paste from native clipboard
 vim.opt.clipboard = "unnamedplus"
