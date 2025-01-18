ad435adffd1ef4da0fae687db3ff058c83aecf5d
jckoh <chong1250_1999@yahoo.com>
2025-01-18 12:29:49 +0800

    Allow multi cursor support

---
+++ lua/plugins/visual-multi.lua
@@ -0,0 +1,8 @@
+return {
+    -- Allow multi cursor select
+    {
+        "mg979/vim-visual-multi",
+        branch = "master", -- Always use the master branch for latest updates
+        lazy = false       -- You can set lazy to true if you want to load it on demand
+    }
+}
