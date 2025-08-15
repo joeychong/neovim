36f6f59c5ca31f3b8d0d4585aacdf672840ab149
Joey Chong <chong1250_1999@yahoo.com>
2025-08-15 23:44:57 +0800

    key mapping for mac

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,7 +8,8 @@ return {
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-      vim.keymap.set('n', '<C-p>', builtin.find_files)
+      vim.keymap.set('n', '<D-p>', builtin.find_files)
+      -- vim.keymap.set('n', '<C-p>', builtin.find_files)
     end,
     opts = {
       defaults = {
@@ -41,7 +42,8 @@ return {
         },
         file_ignore_patterns = {
           "node_modules",
-          "%.git/"
+          "%.git/",
+          ".DS_Store"
         }
       },
       pickers = {
