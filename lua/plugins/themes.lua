93da34becdcd4aaf27b0339282d169622b0b5416
Joey Chong <chong1250_1999@yahoo.com>
2025-11-23 21:12:35 +0800

    add theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -13,5 +13,17 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000
-  }
+  },
+  {
+    "EdenEast/nightfox.nvim"
+  },
+  {
+    'uloco/bluloco.nvim',
+    lazy = false,
+    priority = 1000,
+    dependencies = { 'rktjmp/lush.nvim' },
+    config = function()
+      -- your optional config goes here, see below.
+    end,
+  },
 }
